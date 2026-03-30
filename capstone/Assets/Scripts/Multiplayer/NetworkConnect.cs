d31dd9c01bb97b3d99e9873fe83642ac8353b9bc
kav909 <kavinrajm2008@gmail.com>
2026-03-30 11:48:54 -0500

    s

--- capstone/Assets/Scripts/Multiplayer/NetworkConnect.cs
+++ capstone/Assets/Scripts/Multiplayer/NetworkConnect.cs
@@ -24,7 +24,7 @@ public class NetworkConnect : MonoBehaviour
 
     public async Task CreateRelay(string lobbyId)
     {
-        Allocation allocation = await RelayService.Instance.CreateAllocationAsync(maxConnections, "us-central1");
+        Allocation allocation = await RelayService.Instance.CreateAllocationAsync(maxConnections);
         string newJoinCode = await RelayService.Instance.GetJoinCodeAsync(allocation.AllocationId);
 
         // saves relay code in lobby
