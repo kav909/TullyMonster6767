9148658b1847ae3273705601385436f0049237fa
kav909 <kavinrajm2008@gmail.com>
2026-04-22 11:49:14 -0500

    Update player.cs

--- capstone/Assets/player.cs
+++ capstone/Assets/player.cs
@@ -97,17 +97,18 @@ public class player : MonoBehaviour
             currentDir = "down";
         }
 
-        if (Input.GetKeyDown(KeyCode.Alpha3)) {
+        if (Input.GetKeyDown(KeyCode.Alpha3)&& mana>20) {
             ani.SetTrigger("attack");
             soundManager.GetComponent<soundmanger>().PlaySFX(8);
-           
+            mana -= 20;
             StartCoroutine(useArrow());
         }
-        if (Input.GetKeyDown(KeyCode.Alpha4))
+        if (Input.GetKeyDown(KeyCode.Alpha4)&& mana >10)
         {
             ani.SetTrigger("attack2");
            
             StartCoroutine(PunchAttack());
+            mana -= 10;
         }
 
         if (Input.GetKey(KeyCode.LeftShift) && stamina > 10)
@@ -122,6 +123,7 @@ public class player : MonoBehaviour
         else {
             if(stamina<99)
                 stamina++;
+                
         }
 
             footstepTimer -= Time.deltaTime;
